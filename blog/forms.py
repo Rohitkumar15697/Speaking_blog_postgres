8529bd27164a70bde19d485226fb13f49ee96405
Rohit Kumar <rk221377@gmail.com>
2021-07-09 22:10:34 +0530

    form-control

--- blog/forms.py
+++ blog/forms.py
@@ -3,11 +3,12 @@ from .models import blogpost
 
 
 class Myblogform(forms.ModelForm):
-    choices=(('None','Select you topic'),('Educational','Educational'),('Informational','Informational'),('Funny Thoughts/Jokes','Funny Thoughts/Jokes'),
+    choices=(('None','Select Your Topic'),('Educational','Educational'),('Informational','Informational'),('Funny Thoughts/Jokes','Funny Thoughts/Jokes'),
     ('About Me','About Me'),('Sayri','Sayri'))
-    topic=forms.ChoiceField(choices=choices)
+    topic=forms.ChoiceField(choices=choices,widget=forms.Select(attrs={'class':'form-control'}))
     class Meta:
         model=blogpost
         fields=['topic','title','post']
+        widgets={'title':forms.TextInput(attrs={'class':'form-control'}),'post':forms.Textarea(attrs={'class':'form-control'})}
 
 
