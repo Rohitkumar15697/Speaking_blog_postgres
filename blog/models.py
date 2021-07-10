e1c6b20caf281283a6a319c2d58709fa772d7e06
Rohit Kumar <rk221377@gmail.com>
2021-07-10 21:55:24 +0530

    commit

--- blog/models.py
+++ blog/models.py
@@ -22,12 +22,12 @@ class blogpost(models.Model):
 
 class CommentModel(models.Model):
     post = models.ForeignKey(blogpost ,related_name='comments', on_delete=models.CASCADE)
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return '%s - %s' %(self.post.title, self.date_added)
+        return '%s - %s' %(self.post.title, self.name)
 
     class Meta:
         ordering=['-date_added']
