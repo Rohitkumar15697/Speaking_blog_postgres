6c1758945b892283763bfdbaf9436f0bd42a66f6
Rohit Kumar <rk221377@gmail.com>
2021-07-10 21:44:54 +0530

    commit

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class blogpost(models.Model):
+   
     created_by=models.ForeignKey(User,on_delete=models.CASCADE)
     topic=models.CharField(max_length=122,null=True,blank=False)
     title=models.CharField(max_length=250,blank=False)
@@ -20,14 +21,13 @@ class blogpost(models.Model):
 
 
 class CommentModel(models.Model):
-    
     post = models.ForeignKey(blogpost ,related_name='comments', on_delete=models.CASCADE)
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return '%s - %s' %(self.post.title, self.name)
+        return '%s - %s' %(self.post.title, self.date_added)
 
     class Meta:
         ordering=['-date_added']
