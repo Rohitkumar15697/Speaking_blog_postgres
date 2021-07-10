c8e6de39bdd808820fa6a660a841775a4088e0fe
Rohit Kumar <rk221377@gmail.com>
2021-07-10 10:19:34 +0530

    commit with bootstrap

--- account/form.py
+++ account/form.py
@@ -1,16 +1,19 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.forms import widgets
 
 class createuser(UserCreationForm):
-    password2=forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
-    email=forms.EmailField(required=True)
-    first_name=forms.CharField(required=True)
-    last_name=forms.CharField(required=True)
+    password1=forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class':'form-control','style':'width:400px;'}))
+    password2=forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class':'form-control','style':'width:400px;'}))
+    email=forms.EmailField(required=True, widget=forms.TextInput(attrs={'class':'form-control','style':'width:400px;'}))
+    #first_name=forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control','style':'width:400px;'}))
+    #last_name=forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control','style':'width:400px;'}))
     class Meta:
         model=User
-        fields=['username','password1','password2','email','first_name','last_name']
+        fields=['username','password1','password2','email']
+        widgets={'username':forms.TextInput(attrs={'class':'form-control','style':'width:400px;'})}
 
 class loginform(forms.Form):
-    username=forms.CharField(max_length=122,required=True)
-    password=forms.CharField(max_length=122,widget=forms.PasswordInput,required=True)
+    username=forms.CharField(max_length=122,required=True,widget=forms.TextInput(attrs={'class':'form-control','style':'width:400px;'}))
+    password=forms.CharField(max_length=122,widget=forms.PasswordInput(attrs={'class':'form-control','style':'width:400px;'}),required=True,)
