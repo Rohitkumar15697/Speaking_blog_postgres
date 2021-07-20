fe8b390f4c442fe51c1cba59fc47bb7c3462f70a
Rohit Kumar <rk221377@gmail.com>
2021-07-20 10:41:56 +0530

    email

--- account/views.py
+++ account/views.py
@@ -12,14 +12,16 @@ def register(request):
     if request.method=='POST':
         fm=createuser(request.POST)
         useremail=request.POST['email']
+        username=request.POST['username']
         print(useremail)
-        #Send E-mail
+
+        #Sending Thank you E-mail
         email_subject="From SpeakingBlog"
-        email_message='Thank you for creating account on SpeakingBlog. Now you can post blogs and spread your thoughts. Now login (speakingblog.herokuapp.com/login) and enjoy your with you blogging account'
-                        
+        email_message=f"Thank you for creating account with username ' {username} ' on SpeakingBlog website. Now you can post blogs and spread your thoughts. Go and login (https://speakingblog.herokuapp.com/account/login/) and enjoy your with you blogging account. "                
         email_from=settings.EMAIL_HOST_USER
         email_to=[useremail]
         send_mail(email_subject,email_message,email_from,email_to, fail_silently=False)
+
         if fm.is_valid():
             fm.save()
             messages.info(request,'Account is created successfully, Go to login')
