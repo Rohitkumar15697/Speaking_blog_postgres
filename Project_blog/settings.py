f1dfe7d68485d65225a27ff4f36dbb84b180f5c9
Rohit Kumar <rk221377@gmail.com>
2021-07-21 10:30:57 +0530

    commit with domain

--- Project_blog/settings.py
+++ Project_blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SecretKey')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','speakingblog.herokuapp.com','www.speaksera.co.in','speaksera.co.in','speaksera']
+ALLOWED_HOSTS = ['127.0.0.1','speakingblog.herokuapp.com','www.speaksera.co.in','speaksera.co.in','speaksera.in']
 
 
 # Application definition
