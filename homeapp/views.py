13aafbda88cbcfe8392ecbb5c54f8fe7af96a972
Rohit Kumar <rk221377@gmail.com>
2021-07-24 11:04:05 +0530

    commit

--- homeapp/views.py
+++ homeapp/views.py
@@ -8,7 +8,7 @@ from django.views.generic import ListView,DetailView,DeleteView,UpdateView
 from django.urls import reverse_lazy,reverse
 
 #for counting the number of likes 
-from django.db.models import Count
+from django.db.models import Count, query
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.db.models import Q
@@ -51,8 +51,9 @@ def search_result(request):
 
 class ListData(ListView):
     model=blogpost
-    template_name='blogpost_list.html'
     context_object_name='data'
+    queryset=blogpost.objects.all().order_by('likes')
+    template_name='blogpost_list.html'
 
 
 
