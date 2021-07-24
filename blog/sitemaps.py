ee9d359efef2be681a46408471ec2696e216d468
Rohit Kumar <rk221377@gmail.com>
2021-07-24 10:41:11 +0530

    commit

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -3,5 +3,10 @@ from django.contrib.sitemaps import Sitemap
 from .models import blogpost
 
 class BlogpostSitemap(Sitemap):
+    changefreq='never'
+    priority=0.9
     def items(self):
         return blogpost.objects.all()
+
+    def lastmod(self, obj):
+        return obj.date
