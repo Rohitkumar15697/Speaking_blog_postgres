8f13e89ef36699922bf0cdc3067758e921c7ce19
Rohit Kumar <rk221377@gmail.com>
2021-07-25 19:15:20 +0530

    commit

--- blog/forms.py
+++ blog/forms.py
@@ -6,9 +6,10 @@ class Myblogform(forms.ModelForm):
     choices=(('','Select Topic'),('Educational','Educational'),('Informational','Informational'),('Funny Thoughts/Jokes','Funny Thoughts/Jokes'),
     ('About Me','About Me'),('Sayri','Sayri'),('Other','Other'))
     topic=forms.ChoiceField(choices=choices,widget=forms.Select(attrs={'class':'form-control'}),required=True)
+    
+    
     class Meta:
         model=blogpost
         fields=['topic','title','post']
         widgets={'title':forms.TextInput(attrs={'class':'form-control'}),'post':forms.Textarea(attrs={'class':'form-control', 'style':'height:200px;'})}
-
 
