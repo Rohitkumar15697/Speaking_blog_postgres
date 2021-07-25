bf9618f266deedf26eef5d0d05263b406c97075b
Rohit Kumar <rk221377@gmail.com>
2021-07-25 12:55:18 +0530

    commit

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -2,6 +2,12 @@ import homeapp
 from django.contrib.sitemaps import Sitemap
 from .models import blogpost
 
+class StaticPages(Sitemap):
+    changefreq='daily'
+    priority=0.5
+    def items(self):
+        return ['homeapp:home','homeapp:about']
+
 class BlogpostSitemap(Sitemap):
     changefreq='never'
     priority=0.9
