94689cb985a9e172dfe59a69d43e01b3ee8b44b3
Rohit Kumar <rk221377@gmail.com>
2022-04-15 12:25:01 +0530

    commit

--- account/form.py
+++ account/form.py
@@ -4,10 +4,10 @@ from django import forms
 from django.forms import widgets
 
 class createuser(UserCreationForm):
-    password1 = forms.CharField(label='',required = False,widget=forms.PasswordInput())
-    # password2=forms.CharField(label='',required = False,widget = forms.HiddenInput())
+    password1 = forms.CharField(label='',required = True,widget=forms.PasswordInput())
+    password2=forms.CharField(label='',required = False,widget = forms.HiddenInput())
     email=forms.EmailField(required=True, widget=forms.TextInput(attrs={'class':'form-control','style':'width:400px;'}))
-    first_name=forms.CharField(required=True,label = "Password", widget=forms.PasswordInput(attrs={'class':'form-control','style':'width:400px;'}))
+    first_name=forms.CharField(required=True,label = "Confirm-Password", widget=forms.PasswordInput(attrs={'class':'form-control','style':'width:400px;'}))
     # last_name=forms.CharField(required=True,label = "Confirm-Password", widget=forms.PasswordInput(attrs={'class':'form-control','style':'width:400px;'}))
     class Meta:
         model=User
